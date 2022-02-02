951627f58b8471e035e8429036794d8db32731ce
Danilo Alejandro Ochoa Hidalgo <daochoa6@utpl.edu.ec>
2022-02-02 17:30:36 -0500

    Integración exitosa de Python en MATLAB

--- src/procesamiento.py
+++ src/procesamiento.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 rutaData = './data/'
 rutaDataSource = rutaData + 'COVID-19_mobile_apps.csv'
@@ -11,34 +10,52 @@ rutaGeographicCoverage = rutaDataBase + 'geographicCoverage_'
 def __procesar_fuente_de_datos(rutaDataSource):
 
     # Load DataSource
-    df = pd.read_csv(rutaDataSource, index_col='releaseDate',
+    df = pd.read_csv(rutaDataSource,
+                     index_col='releaseDate',
                      parse_dates=['releaseDate', 'latestUpdate'])
 
     # Drop NaN indexes
     df_not_indexNaN = pd.DataFrame({
-        'releaseDate': df[df.index.isin(df.index.dropna())].index,
-        'name': df['name'][df.index.isin(df.index.dropna())].values,
-        'continents': df['continents'][df.index.isin(df.index.dropna())].values,
-        'geographicCoverage': df['geographicCoverage'][df.index.isin(df.index.dropna())].values
+        'releaseDate':
+        df[df.index.isin(df.index.dropna())].index,
+        'name':
+        df['name'][df.index.isin(df.index.dropna())].values,
+        'continents':
+        df['continents'][df.index.isin(df.index.dropna())].values,
+        'geographicCoverage':
+        df['geographicCoverage'][df.index.isin(df.index.dropna())].values
     }).set_index('releaseDate')
 
     # Format index with uniform datetime format
     pd.DataFrame({
-        'releaseDate': [pd.to_datetime(fecha.strftime('%Y-%m-%d %H:%M:%S')) for fecha in df_not_indexNaN['name'].index],
-        'name': df_not_indexNaN['name'].values,
-        'continents': df['continents'][df.index.isin(df.index.dropna())].values,
-        'geographicCoverage': df['geographicCoverage'][df.index.isin(df.index.dropna())].values
+        'releaseDate': [
+            pd.to_datetime(fecha.strftime('%Y-%m-%d %H:%M:%S'))
+            for fecha in df_not_indexNaN['name'].index
+        ],
+        'name':
+        df_not_indexNaN['name'].values,
+        'continents':
+        df['continents'][df.index.isin(df.index.dropna())].values,
+        'geographicCoverage':
+        df['geographicCoverage'][df.index.isin(df.index.dropna())].values
     }).set_index('releaseDate').sort_index().to_csv(rutaDataWithFormat)
 
     return True
 
 
 def __extraer_cobertura_geografica(df_formated):
-    continents = [continent.title(
-    ) for continent in df_formated['continents'].sort_values().unique().tolist()]
+    continents = [
+        continent.title() for continent in
+        df_formated['continents'].sort_values().unique().tolist()
+    ]
     continents.insert(0, continents.pop(len(continents) - 1))
     continents = pd.DataFrame({
         'continents': continents
-    }).set_index('continents').to_csv(rutaGeographicCoverage + 'continents.csv')
+    }).set_index('continents').to_csv(rutaGeographicCoverage +
+                                      'continents.csv')
 
     return True
+
+
+def imprimir(s):
+    print(s)
